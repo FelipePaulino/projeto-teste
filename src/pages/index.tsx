28851e6db160a5412951abbc1c60e9c69582afd2
felipepaulino <felipepaulino@frwk.com.br>
2022-09-16 16:09:20 -0300

    codigo para teste

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,33 @@
-import type { NextPage } from 'next'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-  return (
-    <div className={styles.container}>
-    dklasjdasjlk
-    </div>
-  )
-}
+  const palavraTeste = "Esse";
 
-export default Home
+  const isPalindromo = (palavra: string) => {
+    const count = palavra.length
+    const initial = 0
+    const divisor = count / 2
+    const firstLetter = palavra[initial].toLocaleLowerCase()
+    const lastLetter = palavra[count - 1].toLocaleLowerCase()
+    const secoundLetter = palavra[initial + 1].toLocaleLowerCase()
+    const lastSecoundLetter = palavra[count - 2].toLocaleLowerCase()
+for(let i = 0; i < palavra.length; i = i + 1 ) {
+    console.log('[for]', palavra[i]);
+}
+    if (firstLetter === lastLetter) {
+return true
+    }
+    return false;
+  };
+  return (
+    <button
+      className={styles.container}
+      onClick={() => isPalindromo(palavraTeste)}
+    >
+      teste
+    </button>
+  );
+};
+
+export default Home;
